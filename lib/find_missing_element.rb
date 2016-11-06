0068b418c90dba64bbb213ba60a789fb22f026c7
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-11-06 15:29:40 -0200

    new version using xor

--- lib/find_missing_element.rb
+++ lib/find_missing_element.rb
@@ -1,12 +1,5 @@
 class FindMissingElement
   def find(array1, array2)
-    array1.sort!
-    array2.sort!
-    # just to not raise exception in last iteration
-    array2 << nil
-    
-    for i in (0..array1.size) do
-      return array1[i] if array1[i] != array2[i]
-    end
+    (array1 + array2).reduce { |result, element| result ^ element }
   end
 end
--- spec/find_missing_element_spec.rb
+++ spec/find_missing_element_spec.rb
@@ -6,4 +6,10 @@ describe FindMissingElement do
       expect(subject.find([4, 7, 1], [7, 4])).to eq 1
     end
   end
+
+  context 'when there are duplications' do
+    it 'returns the missing element' do
+      expect(subject.find([5, 4, 7, 1, 1], [7, 4, 1, 5])).to eq 1
+    end
+  end
 end
