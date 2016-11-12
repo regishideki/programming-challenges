5d16e98b234934c5e7e791c05b7e390349cbff91
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-11-12 17:08:14 -0200

    refactor - extracted graph concept into a separate class

--- lib/transform_word.rb
+++ lib/transform_word.rb
@@ -2,38 +2,61 @@ class TransformWord
   LETTERS = ('a'..'z').to_a
 
   def build_graph(dictionary)
-    graph = Array.new(dictionary.size) { Array.new(dictionary.size) }
-    
-    dictionary.each do |word|
-      word.size.times do |i|
-        word_without_one_char = without_one_char(word, i) 
-        index_word = dictionary.find_index(word)
+    dictionary.each_with_object(Graph.new(dictionary)) do |word, graph|
+      word.size.times do |char_index|
+        word_without_one_char = without_one_char(word, char_index) 
         
         if dictionary.include?(word_without_one_char)
-          index_word_without_one_char = dictionary.index(word_without_one_char)
-          
-          unless index_word_without_one_char.nil?
-            graph[index_word][index_word_without_one_char] = true
-            graph[index_word_without_one_char][index_word] = true
-          end
+          graph.link_words(word, word_without_one_char)
         end
         
         LETTERS.each do |letter|
-          word_with_one_char_changed = word_without_one_char.dup.insert(i, letter)
-          index_word_with_one_char_changed = dictionary.index(word_with_one_char_changed)
+          word_with_one_char_changed = word_without_one_char.dup.insert(char_index, letter)
 
-          if index_word_with_one_char_changed && word_with_one_char_changed != word 
-            graph[index_word][index_word_with_one_char_changed] = true
-            graph[index_word_with_one_char_changed][index_word] = true  
+          if dictionary.include?(word_with_one_char_changed) && word_with_one_char_changed != word 
+            graph.link_words(word, word_with_one_char_changed)
           end 
         end
       end
     end
-    
-    graph
+  end
+
+  private
+
+  def link_words(graph, index_word_1, index_word_2)
+    graph[index_word_1][index_word_2] = graph[index_word_2][index_word_1] = true
   end
 
   def without_one_char(word, char_index_removed)
-    word[0...char_index_removed] + word[char_index_removed + 1..-1]
+    word[0...char_index_removed] + word[(char_index_removed + 1)..-1]
   end
+end
+
+class Graph
+  def initialize(dictionary)
+    @dictionary = dictionary
+    @graph = Array.new(dictionary.size) { Array.new(dictionary.size) }
+  end
+
+  def link_words(w1, w2)
+    index_w1 = dictionary.find_index(w1)
+    index_w2 = dictionary.find_index(w2)
+
+    graph[index_w1][index_w2] = graph[index_w2][index_w1] = true
+  end
+
+  def linked?(w1, w2)
+    index_w1 = dictionary.find_index(w1)
+    index_w2 = dictionary.find_index(w2)
+
+    graph[index_w1][index_w2] && graph[index_w2][index_w1]
+  end
+
+  def to_a
+    @graph
+  end
+
+  private 
+
+  attr_reader :graph, :dictionary
 end
--- spec/transform_word_spec.rb
+++ spec/transform_word_spec.rb
@@ -5,13 +5,8 @@ describe TransformWord do
     let(:dictionary) { ['cat', 'at', 'bat', 'ed', 'bed', 'bet', 'ad'] }
     let(:graph) { subject.build_graph(dictionary) }
 
-    it 'returns a graph with appropriate size' do
-      expect(graph.size).to eq dictionary.size
-      expect(graph[0].size).to eq dictionary.size
-    end
-
     it 'has 8 links' do
-      count = graph.reduce(0) do |result, line|
+      count = graph.to_a.reduce(0) do |result, line|
         result + line.count { |item| item == true }
       end
 
@@ -19,42 +14,35 @@ describe TransformWord do
     end
 
     it 'links "cat" and "at"' do
-      expect(linked?('cat', 'at')).to be true 
+      expect(graph.linked?('cat', 'at')).to be true 
     end
     
     it 'links "at" and "bat"' do
-      expect(linked?('at', 'bat')).to be true
+      expect(graph.linked?('at', 'bat')).to be true
     end
     
     it 'links "ed" and "bed"' do
-      expect(linked?('ed', 'bed')). to be true
+      expect(graph.linked?('ed', 'bed')). to be true
     end
     
     it 'links "cat" and "bat"' do
-      expect(linked?('cat', 'bat')).to be true
+      expect(graph.linked?('cat', 'bat')).to be true
     end
 
     it 'links "bat" and "bet"' do
-      expect(linked?('bat', 'bet')).to be true
+      expect(graph.linked?('bat', 'bet')).to be true
     end
 
     it 'links "bet" and "bed"' do
-      expect(linked?('bet', 'bed')).to be true
+      expect(graph.linked?('bet', 'bed')).to be true
     end
 
     it 'links "at" and "ad"' do
-      expect(linked?('at', 'ad')).to be true
+      expect(graph.linked?('at', 'ad')).to be true
     end
 
     it 'links "ad" and "ed"' do
-      expect(linked?('ad', 'ed')).to be true
+      expect(graph.linked?('ad', 'ed')).to be true
     end
-  end
-  
-  def linked?(w1, w2)
-    index_w1 = dictionary.find_index(w1)
-    index_w2 = dictionary.find_index(w2)
-
-    graph[index_w1][index_w2] && graph[index_w2][index_w1]
   end
 end
