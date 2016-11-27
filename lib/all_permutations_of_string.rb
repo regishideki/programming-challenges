a09a6aff1fd0704088835d83781232782dbf405e
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-11-27 14:21:54 -0200

    remove temp variable

--- lib/all_permutations_of_string.rb
+++ lib/all_permutations_of_string.rb
@@ -7,14 +7,10 @@ class AllPermutationsOfString
     permutations = all_permutations(string[1..-1])
     char = string[0]
 
-    result = Set.new([])
-
-    permutations.each do |permutation|
+    permutations.each_with_object(Set.new([])) do |permutation, result|
       (permutation.size + 1).times do |n|
         result << permutation[0...n] + char + permutation[n..-1]
       end
     end
-
-    result
   end
 end
