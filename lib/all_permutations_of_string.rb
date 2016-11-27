eaddaa8a31fc2273c0df5fe71c8865bae7d73194
Regis Hideki Hattori <regis_hattori@yahoo.com.br>
2016-11-27 14:02:31 -0200

    refactoring

--- lib/all_permutations_of_string.rb
+++ lib/all_permutations_of_string.rb
@@ -2,9 +2,7 @@ require 'set'
 
 class AllPermutationsOfString
   def all_permutations(string)
-    if string.size == 1
-      return [string]
-    end
+    return [string] if string.size == 1
 
     permutations = all_permutations(string[1..-1])
     char = string[0]
@@ -13,7 +11,6 @@ class AllPermutationsOfString
 
     permutations.each do |permutation|
       (permutation.size + 1).times do |n|
-
         result << permutation[0...n] + char + permutation[n..-1]
       end
     end
